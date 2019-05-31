ccd5bd84e94713b2c5a37054435dccc975134851
Samuel Walladge <samuel@swalladge.id.au>
2019-05-31 21:11:06 +0930

    implement all features for dpaste.com
    fixes #3

--- src/backends/dpaste_com.rs
+++ src/backends/dpaste_com.rs
@@ -18,15 +18,32 @@ pub struct Backend {
     #[serde(deserialize_with = "deserialize_url")]
     #[serde(serialize_with = "serialize_url")]
     pub url: Url,
+    pub syntax: Option<String>,
+    pub poster: Option<String>,
+    pub title: Option<String>,
+    #[serde(default)]
+    pub expiry_days: u16,
 }
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "dpaste_com backend")]
 #[structopt(template = "{about}\n\nUSAGE:\n    {usage}\n\n{all-args}")]
 pub struct Opt {
-    /// Url
+    /// Override url in config
     #[structopt(short = "u", long = "url")]
     url: Option<Url>,
+    /// Set to NONE to use default, overriding any set in config file
+    #[structopt(short = "s", long = "syntax")]
+    syntax: Option<String>,
+    /// Set to NONE to use default, overriding any set in config file
+    #[structopt(short = "p", long = "poster")]
+    poster: Option<String>,
+    /// Set to NONE to use default, overriding any set in config file
+    #[structopt(short = "t", long = "title")]
+    title: Option<String>,
+    /// Set to 0 to disable expiry
+    #[structopt(short = "e", long = "expiry-days")]
+    expiry_days: Option<u16>,
 }
 
 pub const NAME: &'static str = "dpaste_com";
@@ -37,13 +54,51 @@ Example config block:
 
     [servers.dpastecom]
     backend = "dpaste_com"
-    url = "http://dpaste.com/""#;
+    url = "http://dpaste.com/"
+
+    # optional; default is plain text
+    # see <http://dpaste.com/api/v2/syntax-choices/> for list of supported names
+    syntax = "js"
+
+    # optional; default is 0 (use server default (7 days)). If not 0, must be in inclusive range 1 - 365.
+    expiry_days = 1
+
+    # optional username to publish as; default is anonymous poster
+    poster = "my name"
+
+    # optional; default is no title
+    title = "my paste"
+"#;
 
 impl PasteClient for Backend {
     fn apply_args(&mut self, args: Vec<String>) -> clap::Result<()> {
         let opt = Opt::from_iter_safe(args)?;
         if let Some(url) = opt.url {
             self.url = url;
+        }
+        if let Some(syntax) = opt.syntax {
+            if syntax == "NONE" {
+                self.syntax = None;
+            } else {
+                self.syntax = Some(syntax);
+            }
+        }
+        if let Some(poster) = opt.poster {
+            if poster == "NONE" {
+                self.poster = None;
+            } else {
+                self.poster = Some(poster);
+            }
+        }
+        if let Some(title) = opt.title {
+            if title == "NONE" {
+                self.title = None;
+            } else {
+                self.title = Some(title);
+            }
+        }
+        if let Some(expiry_days) = opt.expiry_days {
+            self.expiry_days = expiry_days;
         }
         Ok(())
     }
@@ -54,14 +109,33 @@ impl PasteClient for Backend {
         api_endpoint.set_path("/api/v2/");
 
         let form = Form::new().text("content", data);
+        let form = match self.syntax {
+            Some(ref syntax) => form.text("syntax", syntax.to_owned()),
+            None => form,
+        };
+        let form = match self.title {
+            Some(ref title) => form.text("title", title.to_owned()),
+            None => form,
+        };
+        let form = match self.poster {
+            Some(ref poster) => form.text("poster", poster.to_owned()),
+            None => form,
+        };
+        let form = match self.expiry_days {
+            0 => form,
+            e => form.text("expiry_days", e.to_string()),
+        };
+
         let text = Client::new()
             .post(api_endpoint)
             .multipart(form)
             .send()?
             .text()?;
 
-        let url = Url::parse(&text)?;
-        Ok(url)
+        match Url::parse(&text) {
+            Err(e) => Err(format!("could not parse response as url: {}\napi response body: {}", e, text).into()),
+            Ok(url) => Ok(url),
+        }
     }
 }
 
